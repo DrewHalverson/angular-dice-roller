c21b4fb32cd2e0fb0bc0e432aca79670f304bb2c
DrewHalverson <67759475+DrewHalverson@users.noreply.github.com>
2022-10-29 14:46:25 -0500

    Added styling
    imported angular material  and cleaned up spacing with inline styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RollADieComponent } from './roll-a-die/roll-a-die.component';
 import { RollHistoryComponent } from './roll-history/roll-history.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { RollHistoryComponent } from './roll-history/roll-history.component';
     RollHistoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
+    ,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
